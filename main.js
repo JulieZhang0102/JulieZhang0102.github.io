0f0e2eafa0863fe9fc908e17dd346d67e2588e05
JulieZhang0102 <jzhang10@babson.edu>
2020-12-02 00:15:21 -0500

    delete scroll effect

--- main.js
+++ main.js
@@ -4,24 +4,6 @@
     // Animate loader off screen
 		$(".se-pre-con").fadeOut("slow");
     });
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-
-        /* Check the location of each desired element */
-        $('section').slice(1).each( function(i){
-            
-            var desired_object = $(this).position().top + 10;
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is visible in the window, fade it in*/
-            if( bottom_of_window > desired_object ){
-                
-                $(this).animate({'opacity':'1'},1500);
-            
-            }  
-        });
-    });
 
     // When the user scrolls down 1px from the top of the document, resize the navbar's padding and the logo's font size
     // and when scrolls down 50px from the top, display back to top button
